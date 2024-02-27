84d582df477eb0cf3af9e57a794be0896087b349
back2nix <back2nix@list.ru>
2024-02-27 17:10:00 +0300

    save

--- shell.nix
+++ shell.nix
@@ -1,28 +1,28 @@
-{ pkgs ? (
+{
+  pkgs ? (
     let
       sources = import ./nix/sources.nix;
     in
-    import sources.nixpkgs {
-      overlays = [
-        (import "${sources.gomod2nix}/overlay.nix")
-      ];
-    }
-  )
-,
-}:
-let
-  goEnv = pkgs.mkGoEnv { pwd = ./.; };
+      import sources.nixpkgs {
+        overlays = [
+          (import "${sources.gomod2nix}/overlay.nix")
+        ];
+      }
+  ),
+}: let
+  goEnv = pkgs.mkGoEnv {pwd = ./.;};
 in
-pkgs.mkShell {
-  packages = with pkgs; [
-    goEnv
-    pkgs.gomod2nix
-    pkgs.niv
-    translate-shell
-    python310Packages.gtts
-    mpg123
-    libxkbcommon
-    xorg.libX11.dev
-    xorg.libXtst
-  ];
-}
+  pkgs.mkShell {
+    packages = with pkgs; [
+      goEnv
+      pkgs.gomod2nix
+      go-tools
+      pkgs.niv
+      translate-shell
+      python310Packages.gtts
+      mpg123
+      libxkbcommon
+      xorg.libX11.dev
+      xorg.libXtst
+    ];
+  }
